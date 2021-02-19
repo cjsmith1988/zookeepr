4b087201ca8b721bd4edbb1bb5dcfa9314692c17
Curtis Smith <cj1988333@hotmail.com>
2021-02-18 23:39:10 -0500

    finished adding client side feature

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const { runInNewContext } = require('vm');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
 const { animals } = require ('./data/animals');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
